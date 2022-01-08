056eb8f8557a245e6ebeab9d9a0ca34624fe2f37
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-01-08 20:22:32 +0530

    Added Theme

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import Header from "./components/navbar";
+import "./styles/index.css";
+import Header from "./components/header";
 import Footer from "./components/footer";
 import Projects from "./components/projects";
 import About from "./components/about";
@@ -22,3 +22,23 @@ console.log(
 	"%cLanding Home Page Of【Alpha_Wolf_80】Made By【Alpha_Wolf_80】",
 	"color:#2dc9ac;font-weight:900;"
 );
+
+// If no theme found then set a default
+if (!localStorage.getItem("theme")) localStorage.setItem("theme", -1);
+
+let doc_class = document.documentElement.classList;
+if (localStorage.getItem("theme") === "-1") {
+	// Set Theme to User Preference
+	let Utheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+	if (Utheme === "dark") {
+		doc_class.add("dark");
+	} else {
+		doc_class.remove("dark");
+	}
+} else if (localStorage.getItem("theme") === "0") {
+	// Set Theme to Light
+	doc_class.remove("dark");
+} else if (localStorage.getItem("theme") === "1") {
+	// Set Theme to Dark
+	doc_class.add("dark");
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
+const plugin = require("tailwindcss/plugin");
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	darkMode: "class",
 	theme: { extend: {} },
-	plugins: [],
+	plugins: [
+		plugin(function ({ addVariant }) {
+			// Add a `third` variant, ie. `third:pb-0`
+			addVariant("divider", "&[divider]");
+			addVariant("divider-left", '&[divider="left"]');
+			addVariant("divider-right", '&[divider="right"]');
+		}),
+	],
 };
