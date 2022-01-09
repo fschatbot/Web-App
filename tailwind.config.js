7d918172bd74eb498e11adc3a85cd4ac1ea5c6be
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-01-09 08:23:38 +0530

    Fadein animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	darkMode: "class",
-	theme: { extend: {} },
+	theme: {
+		extend: {
+			animation: {
+				FadeIn: "FadeIn 0.5s ease-in-out forwards",
+			},
+			keyframes: {
+				FadeIn: {
+					"0%": { opacity: 0, transform: "translateX(-20px)" },
+					"100%": { opacity: 1, transform: "translateX(0)" },
+				},
+			},
+		},
+	},
 	plugins: [],
 };
