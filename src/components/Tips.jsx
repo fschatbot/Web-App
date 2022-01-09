84c797aad911b0a9d22ba511ec690bdab97eff3f
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-01-09 11:56:28 +0530

    Added mobile support

--- src/components/Tips.jsx
+++ src/components/Tips.jsx
@@ -4,9 +4,9 @@ class Tips extends Component {
 	state = {};
 	render() {
 		return (
-			<div className="text-center text-sm font-medium	text-slate-700/70 dark:text-slate-300/70">
+			<div className="text-center text-sm font-medium text-slate-700/70 dark:text-slate-300/70">
 				<p>Site was made over one weekend on 09-01-2022 when I was 14</p>
-				<p>
+				<p className="hidden md:block">
 					<span className="font-bold text-slate-700 dark:text-slate-300">Tip:</span> Try{" "}
 					<span className="hover:underline cursor-pointer decoration-slate-700 dark:decoration-slate-300">
 						Hovering/Clicking
