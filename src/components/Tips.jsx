2eb16c411f9d83d5e6e2c19e1b241952e9b95a0d
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-01-09 11:53:36 +0530

    Added support for dark

--- src/components/Tips.jsx
+++ src/components/Tips.jsx
@@ -4,11 +4,11 @@ class Tips extends Component {
 	state = {};
 	render() {
 		return (
-			<div className="text-center text-sm font-medium	text-slate-700/70">
+			<div className="text-center text-sm font-medium	text-slate-700/70 dark:text-slate-300/70">
 				<p>Site was made over one weekend on 09-01-2022 when I was 14</p>
 				<p>
-					<span className="font-bold text-slate-700">Tip:</span> Try{" "}
-					<span className="hover:underline cursor-pointer decoration-slate-700">
+					<span className="font-bold text-slate-700 dark:text-slate-300">Tip:</span> Try{" "}
+					<span className="hover:underline cursor-pointer decoration-slate-700 dark:decoration-slate-300">
 						Hovering/Clicking
 					</span>{" "}
 					on any elements to find easter eggs
