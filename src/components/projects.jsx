a1d61877b87e2bbafa37975ffa724c57f8be1f53
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-01-09 19:23:37 +0530

    unused funcitions

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -69,52 +69,6 @@ class Projects extends Component {
 		);
 	}
 
-	DiscordBot() {
-		return (
-			<div className="Project">
-				<LoadImage src="assets/DiscordBot.png" className="Avatar" />
-				<div className="mx-10 my-auto">
-					<h2 className="Title">All in one discord bot made with python</h2>
-					<p className="Description">
-						This bot is a all in one bot. It adds a lot of demanded commands to the server with much
-						more information than any other bots. (The messages can be customized to show
-						minimal/maximal information). It adds the ability to play music, cleanup messages, user
-						info, image manipulation, server management, raid handler, invite tracker, giveaways,
-						memes, quotes, nsfw and much more!! If we miss anything, we add it to the bot.
-					</p>
-					<a
-						href="https://discord.com/api/oauth2/authorize?client_id=809639183374614558&permissions=20835783&scope=bot"
-						target="_blank"
-						rel="noreferrer">
-						Try the bot out yourself!
-					</a>
-				</div>
-			</div>
-		);
-	}
-
-	AdventCalendar() {
-		return (
-			<div className="Project">
-				<div className="mx-10 my-auto">
-					<h2 className="Title">Advent Calendar</h2>
-					<p className="Description">
-						This is a calendar for the advent calendar. It shows the days of the advent calendar and
-						the events that are happening on that day.
-					</p>
-					<a
-						href="
-						https://github.com/fschatbot/Advent-Calendar-Python"
-						target="_blank"
-						rel="noreferrer">
-						Check the Repo For yourself
-					</a>
-				</div>
-				<LoadImage src="assets/AdventOfCode.jpg" className="Avatar" />
-			</div>
-		);
-	}
-
 	Project({ title, description, image_src, link, link_text }) {
 		return (
 			<div className="Project">
