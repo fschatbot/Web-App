23d864ffce8f8ed9eb7f513fb734345f04026c98
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-01-09 11:45:07 +0530

    Added the tips css and text

--- src/components/Tips.jsx
+++ src/components/Tips.jsx
@@ -3,7 +3,18 @@ import React, { Component } from "react";
 class Tips extends Component {
 	state = {};
 	render() {
-		return <p>Site was made over the weekend on 09-01-2022</p>;
+		return (
+			<div className="text-center text-sm font-medium	text-slate-700/70">
+				<p>Site was made over one weekend on 09-01-2022 when I was 14</p>
+				<p>
+					<span className="font-bold text-slate-700">Tip:</span> Try{" "}
+					<span className="hover:underline cursor-pointer decoration-slate-700">
+						Hovering/Clicking
+					</span>{" "}
+					on any elements to find easter eggs
+				</p>
+			</div>
+		);
 	}
 }
 
