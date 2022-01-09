dcf6577b964218e054396a35886a56a03278c38c
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-01-09 06:51:01 +0530

    Added Quotes to the mix

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -17,9 +17,37 @@ class About extends Component {
 						I'm a software engineer. I enjoy coding, playing games and reading. Dreaming of the sky
 						and a prosperous India. Below are few project that were made by me
 					</h3>
+					<br />
+					<blockquote className="font-semibold text-sm md:text-base italic text-gray-700 dark:text-zinc-500">
+						{this.randomQuote()}
+					</blockquote>
 				</div>
 			</div>
 		);
+	}
+
+	randomQuote() {
+		let quotes = [
+			`"It's harder to read code than to write it." - Joel Spolsky`,
+			`“Give a man a program, frustrate him for a day. Teach a man to program, frustrate him for a lifetime.” - Muhammad Waseem`,
+			`“No matter which field of work you want to go in, it is of great importance to learn at least one programming language.” - Ram Ray`,
+			`“I can see into your heart. You have good intentions. You can’t go wrong when you start with good intentions.” - Linda Armstrong`,
+			`“This shows that the ordinary person is wanting more for their lives. They want to be of help, they want to make things better.” - Linda Armstrong`,
+			`“When I can’t find something, I try to remember what I put on top of it. Whatever I was looking for is usually under something else that came next.” - Linda Armstrong`,
+			`“Good software, like wine, takes time.” - Joel Spolsky`,
+			`“Software is a great combination between artistry and engineering.” - Bill Gates`,
+			`“This is a software-powered world.” - Satya Nadella`,
+			`“Nothing works better than just improving your product.” - Joel Spolsky`,
+			`“Writing code is not production, it’s not always craftsmanship though it can be, it’s design.” - Joel Spolsky`,
+			`“Software is the magic thing whose importance only goes up over time.” - Bill Gates`,
+		];
+		return quotes[Math.floor(Math.random() * quotes.length)];
+	}
+
+	componentDidMount() {
+		setInterval(() => {
+			this.forceUpdate();
+		}, 5000);
 	}
 }
 
