c7ea409cfc2fc1b8cacd3ed5d6856236c1f78f92
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-01-09 07:11:29 +0530

    Removed console log

--- src/utils.jsx
+++ src/utils.jsx
@@ -4,7 +4,6 @@ const LoadImage = memo(
 	({ src, alt = "", className = "", delay = 0, loading_src = "loading.gif" }) => {
 		const [currentSrc, updateSrc] = useState(loading_src);
 		useEffect(() => {
-			console.log(delay, typeof delay);
 			const img = new Image();
 			img.src = src;
 			img.onload = () => setTimeout(() => updateSrc(src), delay);
