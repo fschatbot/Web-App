db12ef30653a5c24a9720864ceb0666c742fdadc
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-01-15 15:46:30 +0530

    Added discord banner

--- src/components/discord.jsx
+++ src/components/discord.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
+import "../styles/discord.css";
+import { Link } from "../utils";
 
 class Discord extends Component {
-	state = { InviteURL: "" };
+	state = { InviteURL: "https://discord.com/invite/Tas4unx8rg" };
 	render() {
-		return <div>{this.state.InviteURL}</div>;
+		return (
+			<section className="Discord">
+				<h1>Come Join My Server!!</h1>
+				<Link href={this.state.InviteURL} props={{ className: "no-custom" }}>
+					<img src="assets/discord.svg" alt="" />
+					Join Now
+				</Link>
+			</section>
+		);
 	}
 }
 
