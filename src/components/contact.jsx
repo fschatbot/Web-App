7eb1abf1dffc0030b7c751e5f5fc738a37c21290
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-02-03 21:59:01 +0530

    Added some CSS to the elements

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import "../styles/contact.css";
+import { Link } from "../utils";
 
 class Contact extends Component {
 	state = {};
@@ -9,12 +10,13 @@ class Contact extends Component {
 		let age = new Date(Date.now() - new Date("02/08/2007")).getUTCFullYear() - 1970;
 		return (
 			<div className="Contact">
-				<h1>Few Details that you might need</h1>
-				<ul>
+				<h1 className="Title">Oh so you want to hire me???</h1>
+				<h2 className="Subtitle">Looks like you might need few details, no worries I got you covered!</h2>
+				<ul className="Information">
 					<this.list_item title="Name" content="Himanshu Sultania" />
-					<this.list_item title="Email" content="himanshu.minecraft@gmail.com" />
+					<this.list_item title="Email" content={<Link href="mailto:himanshu.minecraft@gmail.com">himanshu.minecraft@gmail.com</Link>} />
 					<this.list_item title="Age" content={age + " years (8th of Feb 2007)"} />
-					<this.list_item title="Github Username" content="FSChatBot" />
+					<this.list_item title="Github Username" content={<Link href="https://github.com/fschatbot">fschatbot</Link>} />
 					<this.list_item title="Languages I know" content="HTML, CSS, JavaScript, JQuery, TailwindCSS, React, Python, Node.js, Git, CMD, Numpy, JSON, YAML" />
 					<this.list_item title="Location" content="India, Asia Work" />
 					<this.list_item title="Work Preference" content="I like to work remotely and full time! I am a Full Stack Programmer" />
