33a2d41f4a29660def768236eea2fd3a1ba48bf9
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-02-03 20:35:09 +0530

    Some very basic element system

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -4,29 +4,32 @@ import "../styles/contact.css";
 class Contact extends Component {
 	state = {};
 	render() {
+		// https://www.javatpoint.com/calculate-age-using-javascript
+		// The Date string format is MM/DD/YYYY
+		let age = new Date(Date.now() - new Date("02/08/2007")).getUTCFullYear() - 1970;
 		return (
 			<div className="Contact">
 				<h1>Few Details that you might need</h1>
-				<p>
-					Mail: himanshu.minecraft@gmail.com
-					<br />
-					Location: India, Asia Work
-					<br />
-					Prefference: Remote, Full Time
-					<br />
-					Programmer Type: Full Stack Programmer
-					<br />
-					Languages Known: HTML, CSS, JavaScript, JQuery, TailwindCSS, React, Python, Node.js, Git, CMD, Numpy, JSON, YAML
-					<br />
-					Github Username: Fschatbot
-					<br />
-					Name: Himanshu Sultania
-					<br />
-					{/* https://www.javatpoint.com/calculate-age-using-javascript */}
-					{/* The Date string format is MM/DD/YYYY */}
-					Age: {new Date(Date.now() - new Date("02/08/2007")).getUTCFullYear() - 1970} years (8th of Feb 2007)
-				</p>
+				<ul>
+					<this.list_item title="Name" content="Himanshu Sultania" />
+					<this.list_item title="Email" content="himanshu.minecraft@gmail.com" />
+					<this.list_item title="Age" content={age + " years (8th of Feb 2007)"} />
+					<this.list_item title="Github Username" content="FSChatBot" />
+					<this.list_item title="Languages I know" content="HTML, CSS, JavaScript, JQuery, TailwindCSS, React, Python, Node.js, Git, CMD, Numpy, JSON, YAML" />
+					<this.list_item title="Location" content="India, Asia Work" />
+					<this.list_item title="Work Preference" content="I like to work remotely and full time! I am a Full Stack Programmer" />
+					<this.list_item title="Programmer Type" content="I have been working as a software developer for 2 years" />
+				</ul>
 			</div>
+		);
+	}
+
+	list_item({ title, content }) {
+		return (
+			<li>
+				<span className="Contact-Title">{title}: </span>
+				<span className="Contact-Content">{content}</span>
+			</li>
 		);
 	}
 }
