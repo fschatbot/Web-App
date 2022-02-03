cc393a2dfd053d518669378e19197e54fb52993a
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-02-03 22:20:59 +0530

    Instead of texts it now shows images

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -17,7 +17,7 @@ class Contact extends Component {
 					<this.list_item title="Email" content={<Link href="mailto:himanshu.minecraft@gmail.com">himanshu.minecraft@gmail.com</Link>} />
 					<this.list_item title="Age" content={age + " years (8th of Feb 2007)"} />
 					<this.list_item title="Github Username" content={<Link href="https://github.com/fschatbot">fschatbot</Link>} />
-					<this.list_item title="Languages I know" content="HTML, CSS, JavaScript, JQuery, TailwindCSS, React, Python, Node.js, Git, CMD, Numpy, JSON, YAML" />
+					<this.list_item title="Languages I know" content={<this.render_languages />} />
 					<this.list_item title="Location" content="India, Asia Work" />
 					<this.list_item title="Work Preference" content="I like to work remotely and full time! I am a Full Stack Programmer" />
 					<this.list_item title="Programmer Type" content="I have been working as a software developer for 2 years" />
@@ -33,6 +33,32 @@ class Contact extends Component {
 				<span className="Contact-Content">{content}</span>
 			</li>
 		);
+	}
+
+	render_languages() {
+		let languages = {
+			HTML5: "E34F26",
+			CSS3: "1572B6",
+			JavaScript: "F7DF1E",
+			JQuery: "0769AD",
+			TailwindCSS: "06B6D4",
+			React: "09D3AC",
+			Python: "3776AB",
+			"Node.js": "339933",
+			Git: "F05032",
+			Numpy: "013243",
+			JSON: "black",
+			YAML: "black",
+			"Windows Terminal": "4D4D4D",
+			"GNU Bash": "4EAA25",
+		};
+		return Object.keys(languages).map((language) => (
+			<img
+				src={`https://img.shields.io/badge/${language}-${languages[language]}?style=flat-square&logo=${language.toLocaleLowerCase()}&logoColor=white`}
+				className="inline-block mr-1"
+				alt={language}
+			/>
+		));
 	}
 }
 
