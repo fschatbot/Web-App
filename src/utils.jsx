fcab83faf3988af612bb9f7bf69ddda0c453264c
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-03-27 11:01:48 +0530

    Code Beautification

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,16 +1,14 @@
 import { useEffect, useState, memo } from "react";
 
-const LoadImage = memo(
-	({ src, alt = "", className = "", delay = 0, loading_src = "assets/loading.gif" }) => {
-		const [currentSrc, updateSrc] = useState(loading_src);
-		useEffect(() => {
-			const img = new Image();
-			img.src = src;
-			img.onload = () => setTimeout(() => updateSrc(src), delay);
-		}, [src, delay]);
-		return <img src={currentSrc} alt={alt} className={className} />;
-	}
-);
+const LoadImage = memo(({ src, alt = "", className = "", delay = 0, loading_src = "assets/loading.gif" }) => {
+	const [currentSrc, updateSrc] = useState(loading_src);
+	useEffect(() => {
+		const img = new Image();
+		img.src = src;
+		img.onload = () => setTimeout(() => updateSrc(src), delay);
+	}, [src, delay]);
+	return <img src={currentSrc} alt={alt} className={className} />;
+});
 
 const randomint = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
