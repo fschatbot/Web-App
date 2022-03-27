32aace88d43f8278159066fba441fb0ba0a50cd7
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-03-27 10:52:53 +0530

    Code fix, Support Added for mobile phones

--- src/components/notifications.jsx
+++ src/components/notifications.jsx
@@ -8,17 +8,17 @@ class Notification extends Component {
 	render() {
 		return (
 			<div className="Notification">
-				<div>
+				<div className="NotificationTextContainer">
 					{/* Heroicon name: outline/information-circle */}
-					<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline-block mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+					<svg xmlns="http://www.w3.org/2000/svg" className="Notification-Icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 					</svg>
 					Did you know this website's code is open-sourced?
 				</div>
-				<Link href="https://github.com/fschatbot/web-app/" props={{ className: "no-custom hover:bg-sky-200 px-2 py-1 rounded-md font-semibold" }}>
+				<Link href="https://github.com/fschatbot/web-app/" props={{ className: "NotificationRedirect no-custom" }}>
 					Code
 					{/* Heroicon name: outline/arrow-right */}
-					<svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 inline-block ml-1 -translate-y-[2px]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+					<svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" />
 					</svg>
 				</Link>
