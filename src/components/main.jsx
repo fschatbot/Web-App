b1b0f10d5169ed2b7059b68ce110e4d9ae487b19
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-05-17 14:57:16 +0530

    Added credit

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -63,6 +63,7 @@ export default function Main() {
 				<div className="WaveDivider">
 					<About />
 					<Tips />
+					{/* https://www.shapedivider.app/ */}
 					<div className="custom-shape-divider-bottom-1641994563">
 						<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
 							<path
