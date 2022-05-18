5e2ab7f5a1b879314c5daa928bf37ee04a617a68
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-05-18 21:14:56 +0530

    Converted Class Component to Functional Component (Blobs)

--- src/components/blobs.jsx
+++ src/components/blobs.jsx
@@ -1,37 +1,26 @@
-import React, { Component } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import blobshape from "blobshape";
 import dynamics from "dynamics.js";
+import { EasterEggContext } from "../utils";
 
-class Blobs extends Component {
-	state = {
-		count: 0,
+// Convert the class component to functional component
+function Blobs() {
+	let [count, setCount] = useState(0);
+	let { SetEasterEggs } = useContext(EasterEggContext);
+
+	let scaleBlob = () => {
+		const blob = document.getElementById("blobSVG");
+		blob.classList.add("scale-[0.75]");
+		setTimeout(() => blob.classList.remove("scale-[0.75]"), 200);
+
+		// Slimy Friend Achievement
+		console.log(`Blob Obtained! (x${count + 1})`);
+		setCount(count + 1);
+		if (count === 15) SetEasterEggs("Slimy Friend");
 	};
-	render() {
-		return (
-			<div className="Project my-8 text-center Project-Special p-4 md:p-5">
-				<h2 className="Title">Have a blob to help you on your journey!</h2>
-				<div className="mx-auto w-60 h-60 transition-transform inline-block" onMouseEnter={this.startBlob} onMouseLeave={this.stopBlob} onClick={this.scaleBlob} id="blobSVG">
-					<svg viewBox="0 0 100 100">
-						<path d={blobshape({ size: 100, growth: 7, edges: 9 }).path} fill="#2dd4bf" id="blob" />
-					</svg>
-				</div>
-				<p>
-					Powered by{" "}
-					<a href="https://github.com/lokesh-coder/blobshape" target="_blank" rel="noreferrer">
-						blobshape
-					</a>{" "}
-					and{" "}
-					<a href="https://github.com/michaelvillar/dynamics.js" target="_blank" rel="noreferrer">
-						dynamics.js
-					</a>
-				</p>
-			</div>
-		);
-	}
 
-	componentDidMount() {
-		if (this.state.interval) return;
-		let interval = setInterval(() => {
+	useEffect(() => {
+		setInterval(() => {
 			// BLOB
 			const { path } = blobshape({
 				size: 100,
@@ -40,18 +29,28 @@ class Blobs extends Component {
 			});
 			animate(path);
 		}, 1500);
-		this.setState({ interval });
-	}
+	}, []);
 
-	scaleBlob = () => {
-		const blob = document.getElementById("blobSVG");
-		blob.classList.add("scale-[0.75]");
-		setTimeout(() => blob.classList.remove("scale-[0.75]"), 200);
-		// Slimy Friend Achievement
-		console.log(`Blob Obtained! (x${this.state.count + 1})`);
-		this.setState({ count: this.state.count + 1 });
-		if (this.state.count === 15) this.props.SetEasterEggs("Slimy Friend");
-	};
+	return (
+		<div className="Project my-8 text-center Project-Special p-4 md:p-5">
+			<h2 className="Title">Have a blob to help you on your journey!</h2>
+			<div className="mx-auto w-60 h-60 transition-transform inline-block" onClick={scaleBlob} id="blobSVG">
+				<svg viewBox="0 0 100 100">
+					<path d={blobshape({ size: 100, growth: 7, edges: 9 }).path} fill="#2dd4bf" id="blob" />
+				</svg>
+			</div>
+			<p>
+				Powered by{" "}
+				<a href="https://github.com/lokesh-coder/blobshape" target="_blank" rel="noreferrer">
+					blobshape
+				</a>{" "}
+				and{" "}
+				<a href="https://github.com/michaelvillar/dynamics.js" target="_blank" rel="noreferrer">
+					dynamics.js
+				</a>
+			</p>
+		</div>
+	);
 }
 
 const animate = (path, id = "blob") => {
