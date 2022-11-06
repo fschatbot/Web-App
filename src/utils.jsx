912c202d20e17833937a93a0889f4f620379a18a
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-11-06 12:41:53 +0530

    Load Image now has a default

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState, memo, createContext } from "react";
 
-const LoadImage = memo(({ src, alt = "", className = "", delay = 0, loading_src = "assets/loading.gif" }) => {
+const LoadImage = memo(({ src, alt = "", className = "", delay = 0, loading_src = "assets/loading.gif", defaultUrl = "" }) => {
 	const [currentSrc, updateSrc] = useState(loading_src);
 	useEffect(() => {
 		const img = new Image();
 		img.src = src;
 		img.onload = () => setTimeout(() => updateSrc(src), delay);
-	}, [src, delay]);
+		img.onerror = () => (defaultUrl ? setTimeout(() => updateSrc(defaultUrl), delay) : 0);
+	}, [src, delay, defaultUrl]);
 	return <img src={currentSrc} alt={alt} className={className} />;
 });
 
