2e4bc718a1a5023f5e983a832ac053e4b2c8ad6e
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2023-03-12 20:02:05 +0530

    Added a language slider

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -44,20 +44,116 @@ function Contact() {
 	const age = new Date(Date.now() - new Date("02/08/2007")).getUTCFullYear() - 1970;
 	const workingTime = new Date(Date.now() - new Date("02/08/2019")).getUTCFullYear() - 1970;
 	return (
-		<div className="Contact" id="Contact">
-			<h1 className="Title">Oh so you want to hire me???</h1>
-			<h2 className="Subtitle">Looks like you might need few details, no worries I got you covered!</h2>
-			<ul className="Information">
-				<ListItems title="Name" content="Himanshu Sultania" />
-				<ListItems title="Email" content={<Link href="mailto:himanshu.minecraft@gmail.com">himanshu.minecraft@gmail.com</Link>} />
-				<ListItems title="Age" content={`${age} years`} />
-				<ListItems title="Github Username" content={<Link href="https://github.com/fschatbot">fschatbot</Link>} />
-				<ListItems title="Languages I know" content={<KnownLanguages />} />
-				<ListItems title="Location" content="India, Asia Work" />
-				<ListItems title="Work Preference" content="I like to work remotely and full time! I am a Full Stack Programmer" />
-				<ListItems title="Programmer Type" content={`I have been working as a software developer for ${workingTime} years`} />
-			</ul>
+		<>
+			<KnownLanguagesScroll />
+			<div className="Contact" id="Contact">
+				<h1 className="Title">Oh so you want to hire me???</h1>
+				<h2 className="Subtitle">Looks like you might need few details, no worries I got you covered!</h2>
+				<ul className="Information">
+					<ListItems title="Name" content="Himanshu Sultania" />
+					<ListItems title="Email" content={<Link href="mailto:himanshu.minecraft@gmail.com">himanshu.minecraft@gmail.com</Link>} />
+					<ListItems title="Age" content={`${age} years`} />
+					<ListItems title="Github Username" content={<Link href="https://github.com/fschatbot">fschatbot</Link>} />
+					<ListItems title="Languages I know" content={<KnownLanguages />} />
+					<ListItems title="Location" content="India, Asia Work" />
+					<ListItems title="Work Preference" content="I like to work remotely and full time! I am a Full Stack Programmer" />
+					<ListItems title="Programmer Type" content={`I have been working as a software developer for ${workingTime} years`} />
+				</ul>
+			</div>
+		</>
+	);
+}
+
+function KnownLanguagesScroll() {
+	const InfiniteLoopSlider = ({ children, duration, reverse = false }) => {
+		return (
+			<div
+				className="loop-slider"
+				style={{
+					"--duration": `${duration}ms`,
+					"--direction": reverse ? "reverse" : "normal",
+				}}>
+				<div className="inner">
+					{children}
+					{children}
+					{children}
+					{children}
+				</div>
+			</div>
+		);
+	};
+
+	const Tag = ({ language }) => (
+		<div className="tag" style={{ "--color": `#${languageMap[language]}` }}>
+			<img height="32" width="32" src={`https://cdn.simpleicons.org/${language.replace(" ", "").replace(".", "dot")}/${languageMap[language]}`} />
+			{language}
 		</div>
+	);
+
+	/*
+	const languageMap = {
+		HTML5: "E34F26",
+		CSS3: "1572B6",
+		JavaScript: "F7DF1E",
+		JQuery: "0769AD",
+		TailwindCSS: "06B6D4",
+		React: "09D3AC",
+		Python: "3776AB",
+		"Node.js": "339933",
+		Git: "F05032",
+		Numpy: "0393c6",
+		JSON: "e2e8f0",
+		"Windows Terminal": "e2e8f0",
+		"GNU Bash": "4EAA25",
+		Google: "4285F4",
+	};
+	*/
+
+	// Default color: #e2e8f0
+	const languageMap = {
+		languages: {
+			HTML5: "E34F26",
+			CSS3: "1572B6",
+			JavaScript: "F7DF1E",
+			Python: "3776AB",
+			"Node.js": "339933",
+			JSON: "e2e8f0",
+		},
+		frameworks: {
+			React: "09D3AC",
+			TailwindCSS: "06B6D4",
+			JQuery: "0769AD",
+			Numpy: "0393c6",
+			Flask: "e2e8f0",
+		},
+		tools: {
+			Git: "F05032",
+			"Windows Terminal": "e2e8f0",
+			"GNU Bash": "4EAA25",
+			Google: "4285F4",
+		},
+		// design: {
+		// 	Figma: "F24E1E",
+		// },
+	};
+	const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+	return (
+		<>
+			<h1 className="Title text-center">Languages I know</h1>
+			<div className="tag-list">
+				{Object.values(languageMap).map((category, i) => {
+					return (
+						<InfiniteLoopSlider duration={15000 + random(-5000, 5000)} reverse={i % 2}>
+							{Object.keys(category).map((language) => (
+								<Tag language={language} key={language} />
+							))}
+						</InfiniteLoopSlider>
+					);
+				})}
+				<div className="fade" />
+			</div>
+		</>
 	);
 }
 
